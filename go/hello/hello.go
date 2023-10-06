4ccc8709baec19b377d3d57fc6f098ddc7adb1a6
zhengyufei <zhengyufei19999@163.com>
2023-10-06 23:19:55 +0800

    1

--- go/hello/hello.go
+++ go/hello/hello.go
@@ -1,13 +1,28 @@
 package main
 
 import (
-	"example.com/greetings"
 	"fmt"
-	"rsc.io/quote"
+	"log"
+
+	"example.com/greetings"
 )
 
 func main() {
-	fmt.Println(quote.Go())
-	message := greetings.Hello("Gladys")
-	fmt.Println(message)
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
 }
